5bc79f2b45822dab791fd71352fccd7b78147208
ccc <13889915284@163.com>
2019-12-21 23:18:49 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <inp></inp>
-    <list></list>
+    <input type="text" v-model="value">
+    <button @click="submit">submit</button>
+    <ul>
+      <todo-item @delete="hanldeDelete" v-for="(item,index) in list" :key="index" :content="item" :index="index"></todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import Inp from './components/Input'
-import List from './components/List'
+import todoItem from './components/TodoItem'
 export default {
   name: 'app',
-  components: {
-    Inp,
-    List
+  data(){
+    return{
+      list:[],
+      value:''
+    }
+  },
+  components:{
+     'todo-item':todoItem
+  },
+  methods:{
+    submit()
+    {
+      this.list.push(this.value);
+      this.value=''
+    },
+    hanldeDelete(index)
+    {
+      this.list.splice(index,1)
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-}
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <li @click="click">{{content}}</li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props:['content','index'],
+        methods:{
+            click()
+            {
+                this.$emit('delete',this.index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
